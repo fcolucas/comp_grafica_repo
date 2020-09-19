cb0cafa3595d6ff1448a9b9445865c0bb8919d2c
Francisco Lucas <fcolucaslima14@gmail.com>
2020-09-19 14:26:15 -0300

    Atividade Translação

---
+++ Trans por Translação/README.md
@@ -0,0 +1,3 @@
+<h1>Atividade Transformação por Translação</h1>
+
+<p>1. Considere um quadrado com lado unitário no primeiro quadrante, sendo o ponto mais próximo da origem em (1,1). Usando translação, rotação, escala e coordenadas homogêneas, converta esse quadrado em um losango com diagonais com tamanhos iguais a 2 e 4.</p>
---
+++ Trans por Translação/qst1Losango.m
@@ -0,0 +1,24 @@
+function [] = qst1Losango()
+  % coordenadas dos pontos do quadrado
+  % usamos R3 para calcular a translação por uma matriz
+  p1 = [1, 1, 1];
+  p2 = [2, 1, 1];
+  p3 = [2, 2, 1];
+  p4 = [1, 2, 1];
+  
+  quadrado = [p1; p2; p3; p4];
+  Mr = [cos(pi/4) -sin(pi/4) 0; sin(pi/4) cos(pi/4) 0; 0 0 1]; %matriz de rotação a 45º
+  Mt = [1 0 -1; 0 1 -1; 0 0 1]; %matriz de translação
+  Mt2 = [1 0 1; 0 1 1; 0 0 1]; %matriz de translação (final)
+  Me = [sqrt(2) 0 0; 0 2*sqrt(2) 0; 0 0 1]; %matriz de escalonamento
+  
+  quadT = (Mt * quadrado')'; %transladando o quadrado para origem (0,0)
+  quadR = (Mr * quadrado')'; %rotacionando o objeto em 45º anti-horario
+  quadE = (Me * quadR')'; %escalando os pontos das diagonais
+  
+  losango = (Mt2 * quadE')'; %transladando novo objeto para ponto inicial
+  
+  subplot(1, 2, 1), fill(quadrado(:, 1), quadrado(:, 2), 'r'), title("Antes"); 
+  subplot(1, 2, 2), fill(losango(:, 1), losango(:, 2), 'r'), title("Depois");
+  
+endfunction
